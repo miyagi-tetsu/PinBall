4f2ca9deaeb0d1b2228639f9edf00c1f7da3314c
miyagi tetsu <otaji@koneko2006.com>
2018-02-12 23:16:03 +0900

    PinBall 課題提出
    Lesson５  課題、発展課題を行いました。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+
+	private HingeJoint myHingeJoint;
+
+	private float defaultAngle = 20;
+
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		SetAngle(this.defaultAngle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+
+//発展課題
+		foreach (Touch t in Input.touches)
+		{
+			if (t.position.x <= (Screen.width/2)) {
+				if (t.phase == TouchPhase.Began && tag == "LeftFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+
+				if (t.phase == TouchPhase.Ended && tag == "LeftFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}
+
+			if (t.position.x > (Screen.width/2)) {
+				if (t.phase == TouchPhase.Began && tag == "RightFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+
+				if (t.phase == TouchPhase.Ended && tag == "RightFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}
+			
+			}
+
+	}
+
+
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+		
+	}
+}
---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	//得点表示するテキスト
+	private GameObject ScoreText;
+
+	//得点加算用変数
+	public int ScoreCount;
+
+	// Use this for initialization
+	void Start () {
+
+		//シーンのScoreTextオブジェクトを取得
+		this.ScoreText = GameObject.Find("ScoreText");
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+		//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision collision) {
+
+		// タグによって得点を変える
+		if (collision.gameObject.tag == "SmallStarTag") {
+			ScoreCount = ScoreCount + 10;
+		} else if (collision.gameObject.tag == "LargeStarTag") {
+			ScoreCount = ScoreCount + 30;
+		} else if (collision.gameObject.tag == "SmallCloudTag") {
+			ScoreCount = ScoreCount + 20;
+		} else if (collision.gameObject.tag == "LargeCloudTag") {
+			ScoreCount = ScoreCount + 50;
+		}
+
+		//得点を表示
+		this.ScoreText.GetComponent<Text> ().text = ScoreCount.ToString();	
+	}
+
+}
